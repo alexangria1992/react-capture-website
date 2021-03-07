085bb607134da155d68893232f83c8111e391384
alexangria1992 <alex040892@gmail.com>
2021-03-07 21:39:24 +1300

    Finishing Faq Animations

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -2,52 +2,45 @@ import React from 'react'
 import styled from 'styled-components';
 import {About} from '../styles';
 import Toggle from './Toggle';
+import {AnimateSharedLayout} from 'framer-motion'
 
 const FaqSection = () => {
     return (
         <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
-            <Toggle>
-            <div className="question">
-                <h4>How Do I Start?</h4>
+            <AnimateSharedLayout>
+            <Toggle title="How do I start?">
+           
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                      Rerum doloremque placeat eligendi quo quasi laudantium sequi optio eius itaque quae.
                     </p>
                 </div>
-                <div className="faq-line"></div>             
-            </div>
+         
             </Toggle>
-            <div className="question">
-                <h4>Daily Schedule</h4>
+
+            <Toggle title='Daily Schedule'>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, vitae.</p>
+                    </div>
+            </Toggle>
+
+            <Toggle title='Different Payment Methods'>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, vitae.</p>
+                    </div>
+            </Toggle>
+
+            <Toggle title='What Products do you offer?'>
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, vitae.</p>
                 </div>
-                <div className="faq-line"></div>
-            </div>
-
-
-            <div className="question">
-                <h4>Different Payment Method</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, vitae.</p>
-                </div>
-                <div className="faq-line"></div>
-
-            </div>
-
-            <div className="question">
-                <h4>What Products do you offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, vitae.</p>
-                </div>
-                <div className="faq-line"></div>
-
-            </div>
+            </Toggle>
+            </AnimateSharedLayout>
         </Faq>
     )
 }
